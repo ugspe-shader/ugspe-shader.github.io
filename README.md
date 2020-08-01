bebe4ccfa536a0f84251ffd03b8321287faa66e5
Dimas Adiyaksa <66924926+ugspe-shader@users.noreply.github.com>
2020-08-01 15:33:06 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# ugspe.github.io
---
+++ script.js
@@ -0,0 +1,17 @@
+  console.log("Debugger");
+//  var masuk = prompt("Please Read the License in The Website (Type 'Agree')");
+
+  for(i = 0; i < 101; i++){
+    console.clear();
+    console.log("Debug " + i + "%");
+  }
+  
+  console.log("Dev : Dimas Adiyaksa");
+  console.log("Thanks for Download this Shader");
+  
+/*  if(masuk == "Agree"){
+    alert("Welcome To The Website");
+  } else {
+    alert("Please type Agree 😰😰😰");
+    document.location.reload();
+  }*/
